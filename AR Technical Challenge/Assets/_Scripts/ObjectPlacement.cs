8cf0ce1b340fb4e89a718f9225d0b40f0c51ff23
Animesh Sharma <animeshsharma1996@gmail.com>
2021-04-09 02:18:58 +0100

    Object Placing and flags to complete calibration

--- AR Technical Challenge/Assets/_Scripts/ObjectPlacement.cs
+++ AR Technical Challenge/Assets/_Scripts/ObjectPlacement.cs
@@ -1,24 +1,44 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.Experimental.XR;
-using System;
 using UnityEngine.XR.ARSubsystems;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ObjectPlacement : MonoBehaviour
 {
-    [SerializeField] private GameObject placementIndicator;
-    [SerializeField] private GameObject objectToPlace;
+    [SerializeField] private GameObject placementIndicator = null;
+    [SerializeField] private GameObject objectToPlace = null;
+    [SerializeField] private Scene gameScene;
+    [SerializeField] private float rotateSpeed = 0.1f;
+
+    [SerializeField] private GameObject nextButton = null;
+    [SerializeField] private TMP_Text objectPlacedText = null;
+    [SerializeField] private TMP_Text objectRotatedText = null;
+    [SerializeField] private TMP_Text calibrationCompleteText = null;
 
     private ARRaycastManager raycastManager;
     private Pose placementPose;
     private bool isPlacementValid = false;
+    private bool hasInstantiated = false;
+    private bool objectPlaced = false;
+    private bool objectRotated = false;
+    private GameObject placedObject;
 
     private void Start()
     {
+        hasInstantiated = false;
+        objectPlaced = false;
+        objectRotated = false;
+
+        nextButton.SetActive(false);
+        objectPlacedText.gameObject.SetActive(false);
+        objectRotatedText.gameObject.SetActive(false);
+        calibrationCompleteText.gameObject.SetActive(false);
+
         raycastManager = FindObjectOfType<ARRaycastManager>();
     }
+
 
     private void Update()
     {
@@ -29,9 +49,73 @@ public class ObjectPlacement : MonoBehaviour
 
     private void UpdateObjectPlacement()
     {
-        if(isPlacementValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (isPlacementValid && Input.touchCount > 0)
         {
-            Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+            Touch touch = Input.GetTouch(0);
+            if (!hasInstantiated)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    placedObject = Instantiate(objectToPlace);
+                    hasInstantiated = true;
+
+                    if (!objectPlaced)
+                    {
+                        placedObject.transform.position = placementPose.position;
+                        if (touch.phase == TouchPhase.Ended)
+                        {
+                            objectPlacedText.gameObject.SetActive(true);
+                            objectPlaced = true;
+                        }
+                    }
+                }
+
+                if (objectPlaced && !objectRotated && touch.phase == TouchPhase.Began)
+                {
+                    RotateObject(touch);
+                    objectRotatedText.gameObject.SetActive(true);
+                    calibrationCompleteText.gameObject.SetActive(true);
+                    nextButton.SetActive(true);
+                    objectRotated = true;
+                }
+            }
+            else
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    if (!objectPlaced)
+                    {
+                        placedObject.transform.position = placementPose.position;
+                        if (touch.phase == TouchPhase.Ended)
+                        {
+                            objectPlacedText.gameObject.SetActive(true);
+                            objectPlaced = true;
+                        }
+                    }
+                }
+
+                if (objectPlaced && !objectRotated && touch.phase == TouchPhase.Began)
+                {
+                    RotateObject(touch);
+                    objectRotatedText.gameObject.SetActive(true);
+                    calibrationCompleteText.gameObject.SetActive(true);
+                    nextButton.SetActive(true);
+                    objectRotated = true;
+                }
+            }
+        }
+    }
+
+    private void RotateObject(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Moved)
+        {
+            Quaternion rotation = Quaternion.Euler(-touch.deltaPosition.x * rotateSpeed, -touch.deltaPosition.y * rotateSpeed, 0f);
+            placedObject.transform.rotation = rotation * transform.rotation;
+        }
+        else
+        {
+            placedObject.transform.rotation = placementPose.rotation;
         }
     }
 
@@ -50,9 +134,9 @@ public class ObjectPlacement : MonoBehaviour
 
     private void UpdatePlacementPose()
     {
-        Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f,0.5f));
+        Vector3 screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f,0.5f));
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        raycastManager.Raycast(screenCenter, hits, TrackableType.PlaneEstimated);
 
         isPlacementValid = hits.Count > 0;
         if(isPlacementValid)
